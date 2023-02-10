7c8fc087a5761a26b1b7e3ab9ecf3baf67a7b452
Amanjot Singh <108488845+Amanjot4Singh@users.noreply.github.com>
2023-02-10 16:04:52 +0530

    Add files via upload

---
+++ Cpp/7bLoopsQuesA.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>//find sum of all even numbers
+using namespace std;
+
+int main(){
+    int n,s;
+    cout<<" Enter an integer: ";
+    cin>>n;
+    s=n*(n+1);
+    cout<<" Sum of "<<n<<" even integers is : "<<s<<endl;
+}
