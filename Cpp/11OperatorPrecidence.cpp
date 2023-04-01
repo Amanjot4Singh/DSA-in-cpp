e2e658e8c6f572bfe7db4fe5b28e9416cd5195d6
Amanjot Singh <108488845+Amanjot4Singh@users.noreply.github.com>
2023-04-02 02:34:15 +0530

    Add files via upload

---
+++ Cpp/11OperatorPrecidence.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout<<"2*3/4+5 = "<<2*3/4+5<<endl;//here first 2*# becomes 6 then 6/4 becomes 1 as 1.5 because int to 1 and then +5 to it : 6
+    cout<<"2*(3/4)+5 = "<<2*(3/4)+5<<endl;//here first (3/4) becomes 0 as 0.75 because int to 0 and then +5 to it : 5
+}
